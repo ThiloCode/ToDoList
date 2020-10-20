3100f0b5052360ab5d1a6a484a22c05ebf4e3cee
ThiloG <thilo5@hotmail.com>
2020-10-20 12:24:56 +0100

    Added brief timed reward message

--- src/AchieveItemCommand.java
+++ src/AchieveItemCommand.java
@@ -1,13 +1,24 @@
+import java.util.concurrent.TimeUnit;
 
 public class AchieveItemCommand implements Command{
 	private ToDoList toDoList;
+	
+	private String[] praise = {"Good Job!", "Nice work!", "Woohoo!"};
+	
 	
 	public AchieveItemCommand(ToDoList toDoList){
 		this.toDoList = toDoList;
 	}
 	
 	public void execute(){
-		ScreenManager.displayStatusMessage("Woohoo!");
+		
+		ScreenManager.displayStatusMessage(praise[(int)(Math.random() * 3)]);
 		toDoList.achieveItem();
+		
+		try {
+			TimeUnit.SECONDS.sleep((long)(1));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
